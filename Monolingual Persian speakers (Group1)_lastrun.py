af417535ac3a25ad6520882507262433c7038355
Janan Shalpoush <jananshalpoush@gmail.com>
2024-08-12 11:18:03 +0900

    _

--- Monolingual Persian speakers (Group1)_lastrun.py
+++ Monolingual Persian speakers (Group1)_lastrun.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 """
 This experiment was created using PsychoPy3 Experiment Builder (v2024.1.5),
-    on Sun Aug  4 14:51:18 2024
+    on Mon Aug 12 11:16:55 2024
 If you publish work using this script the most relevant publication is:
 
     Peirce J, Gray JR, Simpson S, MacAskill M, Höchenberger R, Sogo H, Kastman E, Lindeløv JK. (2019) 
